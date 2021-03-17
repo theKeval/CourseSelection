3c12b80029b3378d6b5ec1568c4e43bb32e72025
Keval <keval.langalia@live.com>
2021-03-18 02:06:15 +0530

    working on add course to program button

--- CourseSelection/ViewController.swift
+++ CourseSelection/ViewController.swift
@@ -31,8 +31,22 @@ class ViewController: UIViewController {
 
     @IBAction func addCourseToProgram(_ sender: UIButton) {
         
-        if let _ = selectedCourse {
+        if let course = selectedCourse {
             // add course to selected course list
+            let contains = MyData.selectedCourses.contains { (_course) -> Bool in
+                _course.courseName == course.courseName
+            }
+            
+            if contains{
+                showAlertToast(message: "You have already selected this course previously!", seconds: 4)
+                // showCustomToast(message: "You have already selected this course previously.", font: .systemFont(ofSize: 17))
+            }
+            else {
+                MyData.selectedCourses.append(course)
+                showAlertToast(message: "Course added to program!", seconds: 3)
+                // showCustomToast(message: "Course added to Program", font: .systemFont(ofSize: 17))
+            }
+            
         }
         
     }
@@ -63,4 +77,39 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
 }
+
+extension UIViewController{
+
+    func showAlertToast(message : String, seconds: Double){
+        let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+        alert.view.backgroundColor = .darkGray
+        alert.view.alpha = 1
+        alert.view.layer.cornerRadius = 15
+        self.present(alert, animated: true)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds) {
+            alert.dismiss(animated: true)
+        }
+    }
+    
+    func showCustomToast(message : String, font: UIFont) {
+
+        let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+        toastLabel.textColor = UIColor.white
+        toastLabel.font = font
+        toastLabel.textAlignment = .center;
+        toastLabel.text = message
+        toastLabel.alpha = 1.0
+        toastLabel.layer.cornerRadius = 10;
+        toastLabel.clipsToBounds  =  true
+        self.view.addSubview(toastLabel)
+        UIView.animate(withDuration: 0.5, delay: 2.0, options: .curveEaseOut, animations: {
+             toastLabel.alpha = 0.0
+        }, completion: {(isCompleted) in
+            toastLabel.removeFromSuperview()
+        })
+    }
+    
+    
+ }
 
