d7d51dee09f6f5e9c44a4bc708acab835635a59a
Keval <keval.langalia@live.com>
2021-03-18 01:06:35 +0530

    UI of selectedCourse screen -> almost done

---
+++ CourseSelection/SelectedCoursesTableViewCell.swift
@@ -0,0 +1,38 @@
+//
+//  SelectedCoursesTableViewCell.swift
+//  CourseSelection
+//
+//  Created by Keval on 3/18/21.
+//
+
+import UIKit
+
+class SelectedCoursesTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var uiCourseImage: UIImageView!
+    @IBOutlet weak var uiCourseName: UILabel!
+    @IBOutlet weak var uiHourAndPrice: UILabel!
+    @IBOutlet weak var btnDelete: UIButton!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    func setCourseData(course: CourseModel) {
+        self.uiCourseImage.image = UIImage(named: course.courseImage)
+        self.uiCourseName.text = course.courseName
+        self.uiHourAndPrice.text = "\(course.hours) Hours, $ \(course.fee)"
+    }
+
+    @IBAction func deleteCourse(_ sender: UIButton) {
+        
+    }
+    
+}
--- CourseSelection/SelectedCoursesViewController.swift
+++ CourseSelection/SelectedCoursesViewController.swift
@@ -8,11 +8,20 @@
 import UIKit
 
 class SelectedCoursesViewController: UIViewController {
+    
+    @IBOutlet weak var uiTotalHours: UILabel!
+    @IBOutlet weak var uiTotalFees: UILabel!
+    @IBOutlet weak var tvSelectedCourses: UITableView!
+    @IBOutlet weak var btnBack: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        // Setting up the initial UI
+        tvSelectedCourses.delegate = self
+        tvSelectedCourses.dataSource = self
+        tvSelectedCourses.tableFooterView = UIView()
     }
     
 
@@ -25,5 +34,31 @@ class SelectedCoursesViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func goBack(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
 
+}
+
+extension SelectedCoursesViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        MyData.selectedCourses.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let selectedCourse = MyData.selectedCourses[indexPath.row]
+        
+        let cell = tvSelectedCourses.dequeueReusableCell(withIdentifier: "tvcell_selectedCourses") as! SelectedCoursesTableViewCell
+        cell.setCourseData(course: selectedCourse)
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // code to execute on row selection
+    }
+    
 }
